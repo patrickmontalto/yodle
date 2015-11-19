129e8c8258a4a8ce4497b437d35d172f73e4d271
patrickmontalto <pmontalto@gmail.com>
2015-11-19 10:54:38 -0500

    finish juggle solver

--- juggle/juggler_solver.rb
+++ juggle/juggler_solver.rb
@@ -1,6 +1,8 @@
 class JugglerSolver
   require 'matrix'
+  # create instance variables to be used throughout the program
   def initialize
+    @fname = "jugglefest.txt"
     @circuits = []
     @jugglers = []
     @queue = []
@@ -10,9 +12,10 @@ class JugglerSolver
     @circuit_jugglers = {} 
     @c = {}
     @j = {}
-    @file = File.open("jugglefest.txt")
+    @file = File.open(@fname)
   end
 
+  # read .txt file into program and populate necessary hashes and lists
   def read_file(file, circuits, jugglers, c, j)
     # read each line so that it can be made into a hash of circuits and jugglers
     file.each do |line|
@@ -29,15 +32,14 @@ class JugglerSolver
     end
 
     # create a hash of circuits with an array of jugglers. this will be used to store the results.
-    circuit_jugglers = {}
-    c.each_key { |key| circuit_jugglers[key] = [] }
+    c.each_key { |key| @circuit_jugglers[key] = [] }
 
     # calculate target value in each circuit
     @target = @jugglers.count / @circuits.count
   end
 
+  # add each juggler to @juggler_list as a hash containing their preferences and scores
   def build_jugglers_list(j, c)
-    # add each juggler to @juggler_list as a hash {id: "J01", scores: [{C02: 112},{C01: 110},{C05: 93}], current_pref: 0}
     j.each do |key, values|
       circuit_scores = []
       values[:preferences].each do |pref|
@@ -51,6 +53,9 @@ class JugglerSolver
     @queue = @juggler_list
   end
 
+  # assign jugglers to teams based on their current preference
+  # if they have no more preferences then put them in the leftover
+  # array
   def assign_jugglers(queue, leftover, circuit_jugglers)
     until (juggler = queue.shift) == nil
       unless juggler[:scores][juggler[:current_pref]] == nil
@@ -62,12 +67,18 @@ class JugglerSolver
     end
   end
 
+  # sort jugglers in descending score order. use preference
+  # as a tie-breaker for even scores.
   def sort_jugglers(circuit_jugglers)
     circuit_jugglers.each do |course, jugglers|
       sorted_jugglers = jugglers.sort! { |x,y|
+        # get x juggler's score
         jug_x_score = x[:scores][y[:current_pref]].values[0]
+        # get y juggler's score
         jug_y_score = y[:scores][y[:current_pref]].values[0]
 
+        # if the scores are equal, the juggler who's preference for this course
+        # is higher wins
         if jug_y_score == jug_x_score 
           y[:current_pref] <=> x[:current_pref]
         else
@@ -77,6 +88,8 @@ class JugglerSolver
     end
   end
 
+  # bump jugglers if they exceed the limit for the circuit team
+  # add them back to the queue when bumped
   def bump_jugglers(circuit_jugglers, queue)
     circuit_jugglers.each do |course, jugglers|
       if jugglers.count > @target
@@ -90,6 +103,7 @@ class JugglerSolver
     end
   end
 
+  # compute the sum of juggler id #'s for a given circuit team
   def find_sum(circuit, circuit_jugglers)
     sum = 0
     circuit_jugglers[circuit].each do |juggler|
@@ -98,11 +112,12 @@ class JugglerSolver
     puts "The sum of #{circuit}'s juggler ID's is #{sum}"
   end
 
+  # write the output to a text file with the proper formatting
   def write_output(circuit_jugglers)
     fname = "output.txt"
     file = File.open(fname, "w")
 
-    circuit_jugglers.each do |key, course|
+    circuit_jugglers.to_a.reverse.to_h.each do |key, course|
       jugglers = []
       0.upto(circuit_jugglers[key].size - 1) do |juggler|
         juggler_id = course[juggler][:id]
@@ -112,7 +127,7 @@ class JugglerSolver
           score = entry.values[0]
           juggler_scores << "#{course_no}:#{score}"
         end
-        jugglers << "#{juggler_id} #{juggler_scores.join(", ")}"
+        jugglers << "#{juggler_id} #{juggler_scores.join(", ").gsub(",", "")}"
       end
       file.puts "#{key} #{jugglers.join(", ")}"
     end
@@ -121,16 +136,20 @@ class JugglerSolver
     puts "Wrote output to output.txt"
   end
 
+  # solve the problem
   def solve
+    puts "reading file and building lists..."
     read_file(@file, @circuits, @jugglers, @c, @j)
     build_jugglers_list(@j, @c)
+    puts "solving problem..."
     while @queue.count > 0
       assign_jugglers(@queue, @leftover, @circuit_jugglers)
       sort_jugglers(@circuit_jugglers)
       bump_jugglers(@circuit_jugglers, @queue)
-      find_sum("C1970", @circuit_jugglers)
-      write_output(@circuit_jugglers)
     end
+    puts "done."
+    write_output(@circuit_jugglers)
+    find_sum("C1970", @circuit_jugglers) if @fname == "jugglefest.txt"
   end
 
 end
